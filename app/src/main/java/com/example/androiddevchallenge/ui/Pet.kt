9711819ddd420bf6af6dd6accdb91e3d3749264b
Pon <tyanpon57@gmail.com>
2021-02-27 16:24:25 +0900

    app:spotlessApply した

--- app/src/main/java/com/example/androiddevchallenge/ui/Pet.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/Pet.kt
@@ -1,26 +1,35 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.navigate
-import androidx.navigation.compose.rememberNavController
-import com.example.androiddevchallenge.App
 import com.example.androiddevchallenge.entity.Cat
 import com.example.androiddevchallenge.entity.Dog
 import com.example.androiddevchallenge.entity.Pet
-import com.example.androiddevchallenge.ui.theme.MyTheme
 
 val dummyPetsList: List<Pet> = listOf(
     Dog("poti", 2),
@@ -46,7 +55,7 @@ fun PetsList(navController: NavController, pets: List<Pet>, modifier: Modifier =
                                 when (pet) {
                                     is Dog -> Text(text = "\uD83D\uDC36")
                                     is Cat -> Text(text = "\uD83D\uDE40")
-                                }// Icons.Filled.Star
+                                } // Icons.Filled.Star
                                 Text(text = "name: ${pet.name} age: ${pet.age}")
                             }
                         }
